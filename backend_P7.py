8462beb1579b94ba93fc3c38ec511fdca0a9d56d
Isabel64450 <152506194+Isabel64450@users.noreply.github.com>
2024-04-20 22:22:13 +0200

    Update backend_P7.py
    change the port

--- backend_P7.py
+++ backend_P7.py
@@ -60,5 +60,5 @@ def predict():
          
     
 if __name__ == "__main__":
-    port = os.environ.get("PORT", 5000)
-    app.run(debug=True, host="0.0.0.0", port=int(port))
+    #port = os.environ.get("PORT", 5000)
+    app.run(debug=True, host="0.0.0.0", port=8080)
